e2e33d3cfc7ca58b3199863a13298f7e78864106
msierra <manuelsierramunoz@gmail.com>
2026-07-16 17:09:17 +0200

    change api CORS constant

--- apps/api/src/config/constants.ts
+++ apps/api/src/config/constants.ts
@@ -1,5 +1,3 @@
 export const DEVORIGINS = ["http://localhost:3000", "http://localhost:3001"]
 
-export const ORIGINS = process.env.ORIGINS?.split(",") ?? [
-  "https://agropilotapp.es",
-]
+export const ORIGINS = ["https://agropilotapp.es"]
