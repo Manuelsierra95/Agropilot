314e2355d3a1a4e82c931954840c8997ba89d8a6
msierra <manuelsierramunoz@gmail.com>
2026-08-17 18:47:46 +0200

    fix drizzle config

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-const databaseUrl = process.env.DATABASE
+const databaseUrl = process.env.DATABASE_URL
 
 if (!databaseUrl) {
   throw new Error("Missing database URL. Set DATABASE_URL.")
